c3dc8bfeae59591d3d022753aa027b3797c5b9df
pyRamsd <minauzum56@gmail.com>
2024-04-27 17:23:13 -0500

    imagen link compartida

--- ComandosDeLinux/ComandosDeLinux.py
+++ ComandosDeLinux/ComandosDeLinux.py
@@ -18,7 +18,7 @@ def content():
 @rx.page(
         title="Comandos de linux by Sergio Ruiz",
         description="Documentación de comandos linux para distintos propósitos de desarrollo",
-        image="/assests/logo.jpg"
+        image="/assets/logo.jpg"
 )
 def index() -> rx.Component:
     return rx.center(
