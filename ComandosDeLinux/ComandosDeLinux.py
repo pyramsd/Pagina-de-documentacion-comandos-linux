f7cdf1a5d80a0cebcc841b991944541e3fc07a8c
pyRamsd <minauzum56@gmail.com>
2024-05-01 09:57:01 -0500

    1/05/2024

--- ComandosDeLinux/ComandosDeLinux.py
+++ ComandosDeLinux/ComandosDeLinux.py
@@ -2,6 +2,7 @@ import reflex as rx
 from ComandosDeLinux.styles.styles import BASE_STYLE
 from ComandosDeLinux.pages.index import index
 from ComandosDeLinux.pages.quiz import quiz
+from ComandosDeLinux.pages.results import result
 
 app = rx.App(
     style=BASE_STYLE,
